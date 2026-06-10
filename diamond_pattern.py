afe278c69755b6b703516b82ef62f1a2261dac21
Sravanthi653 <sravanthisolleti48@gmail.com>
2026-06-10 14:54:02 +0530

    Diamond_Pattern

--- diamond_pattern.py
+++ diamond_pattern.py
@@ -1,15 +1,27 @@
-rows=5
-for i in range(1,rows+1):
-    res=" "
-    for sp in range(1,rows-i+1):
-        res+=" "
-    for j in range(1,i+1):
-        res+="*" +" "
-    print(res) 
-for i in range(rows-1,0,-1):
-    res=" "
-    for sp in range(rows-i):
-        res+=" "
-    for j in range(i):
-        res+="*"+" "
-    print(res)   
+# Pyramid Pattern
+height = 5
+
+for i in range(1, height + 1):
+    print("*" * i)
+
+print()
+
+# Square Pattern
+size = 4
+char = "#"
+
+for i in range(size):
+    print(char * size)
+
+print()
+
+# Diamond Pattern
+width = 5
+
+for i in range(1, width + 1, 2):
+    spaces = (width - i) // 2
+    print(" " * spaces + "*" * i)
+
+for i in range(width - 2, 0, -2):
+    spaces = (width - i) // 2
+    print(" " * spaces + "*" * i)
